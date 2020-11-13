2a370f923f2332d965c0faed4e011b8a29d85e23
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-11-13 23:04:31 +0100

    Creato il file della schermata di login.

---
+++ lib/interface/screen/login_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  static const String route = "/loginScreen";
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
