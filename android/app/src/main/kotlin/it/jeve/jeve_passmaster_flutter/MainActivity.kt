c1e11415837a3f6669cf7c28bf8b6adaaa382319
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-11-13 23:03:37 +0100

    Creato il progetto Flutter.

---
+++ android/app/src/main/kotlin/it/jeve/jeve_passmaster_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package it.jeve.jeve_passmaster_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Passmaster());
+}
+
+class Passmaster extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
