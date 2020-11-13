ac2b2ec1df32bc7db3cb0c569bc7bb4c746f8c14
Emilio Dalla Torre <emiliodallatorre12@live.com>
2020-11-13 23:24:26 +0100

    Implementata la struttura di base del progetto.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:jeve_passmaster_flutter/generated/l10n.dart';
+import 'package:jeve_passmaster_flutter/interface/screen/login_screen.dart';
+import 'package:jeve_passmaster_flutter/references.dart';
 
 void main() {
   runApp(Passmaster());
@@ -8,12 +12,21 @@ class Passmaster extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: References.appName,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      supportedLocales: S.delegate.supportedLocales,
+      localizationsDelegates: <LocalizationsDelegate>[
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      initialRoute: LoginScreen.route,
+      routes: <String, Widget Function(BuildContext)>{
+        LoginScreen.route: (BuildContext context) => LoginScreen(),
+      },
     );
   }
 }
-
